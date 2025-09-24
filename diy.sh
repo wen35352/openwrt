bcb5649353e84da889b1843218ca5b7c92ead65f
wen35352 <2264966032@qq.com>
2025-09-25 00:24:19 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -18,5 +18,4 @@ echo 'src-git UA3F https://github.com/SunBK201/UA3F.git' >>feeds.conf.default
 #git clone https://github.com/kenzok8/small.git package/small
 #git clone https://github.com/SunBK201/UA3F.git package/UA3F
 git clone https://github.com/stevenjoezhang/luci-app-adguardhome.git package/ADGH
-echo 'src-git qmodem https://github.com/FUjr/QModem.git;main' >> feeds.conf.default
-git clone https://github.com/ofmodemsandmen/ROOterSource.git
+echo 'src-git qmodem https://github.com/FUjr/QModem.git;main
